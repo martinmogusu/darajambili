07344039d7b7e0c978faaf8edb7748668a4e7f41
Martin Mogusu <martinmogusu@gmail.com>
2021-07-09 15:11:51 +0300

    Updated wsgi.py

--- daraja_listener/wsgi.py
+++ daraja_listener/wsgi.py
@@ -9,5 +9,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "daraja_listener.settings")
+
+application = get_wsgi_application()
 
